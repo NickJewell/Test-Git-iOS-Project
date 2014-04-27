f30d40758dab2b3eb1b7d1658bf4b3238bbb6c7c
Nick Jewell <nick.jewell@gmail.com>
2014-04-27 22:33:38 +0100

    Added test class

---
+++ MacJTest.h
@@ -0,0 +1,13 @@
+//
+//  MacJTest.h
+//  Git Test
+//
+//  Created by Nick Jewell on 27/04/2014.
+//  Copyright (c) 2014 MacJewell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MacJTest : NSObject
+
+@end
---
+++ MacJTest.m
@@ -0,0 +1,13 @@
+//
+//  MacJTest.m
+//  Git Test
+//
+//  Created by Nick Jewell on 27/04/2014.
+//  Copyright (c) 2014 MacJewell. All rights reserved.
+//
+
+#import "MacJTest.h"
+
+@implementation MacJTest
+
+@end
